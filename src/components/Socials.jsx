2ecb2101998d95b19a26834975db7cef309a5904
Favour Shaw <favourvanderwaal@gmail.com>
2025-10-04 23:28:20 +0100

    Enhance Socials component by adding Gmail icon and updating styles for better responsiveness and visual appeal

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,12 +3,13 @@ import Twitter from "../assets/icons/twitter.svg";
 import Instagram from "../assets/icons/insta.svg";
 import Git from "../assets/icons/github.svg";
 import Whatsapp from "../assets/icons/whatsapp.svg";
+import Gmail from "../assets/icons/gmail.svg";
 
 const Socials = () => {
   return (
     <div>
       {" "}
-      <div className="relative flex items-center justify-center w-[200px] h-[50px] overflow-hidden bg-gray-200 shadow-md transition-all duration-300 group font-medium rounded-lg text-sm ">
+      <div className="relative flex items-center justify-center w-[250px] h-[50px] overflow-hidden bg-gradient-to-l from-[#00c6ff] to-gray-300 shadow-md transition-all duration-300 group font-medium rounded-lg text-sm">
         <div className="absolute left-0 flex items-center justify-end w-1/2 h-full bg-gradient-to-l from-[#00c6ff] to-[#0072ff] transition-transform duration-300 group-hover:-translate-y-full opacity-70" />
         <div className="absolute right-0 flex items-center justify-start w-1/2 h-full bg-gradient-to-r from-[#00c6ff] to-[#0072ff] transition-transform duration-300 group-hover:translate-y-full opacity-70" />
 
@@ -16,30 +17,36 @@ const Socials = () => {
           Socials
         </span>
 
-        <div className="relative z-20 flex items-center justify-around w-full h-full opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <div className="relative z-20 flex items-center justify-around w-full h-full opacity-0 pointer-events-none transition-all duration-300 group-hover:opacity-100 group-hover:pointer-events-auto">
           <a
             href="#"
-            className="flex items-center justify-center w-1/4 h-full transition hover:bg-white/70 hover:scale-110"
+            className="flex items-center justify-center w-1/5 h-full transition hover:bg-blue-300 hover:scale-110"
           >
             <img src={Twitter} alt="Twitter" className="w-6 h-6" />
           </a>
           <a
             href="https://wa.me/2348167679576"
-            className="flex items-center justify-center w-1/4 h-full transition hover:bg-white/70 hover:scale-110"
+            className="flex items-center justify-center w-1/5 h-full transition hover:bg-green-200 hover:scale-110"
           >
             <img src={Whatsapp} alt="Whatsapp" className="w-6 h-6" />
           </a>
           <a
             href="https://www.instagram.com/senor_shaw/"
-            className="flex items-center justify-center w-1/4 h-full transition hover:bg-white/70 hover:scale-110"
+            className="flex items-center justify-center w-1/5 h-full transition hover:bg-red-300 hover:scale-110"
           >
-            <img src={Instagram} alt="Insta" className="w-6 h-6" />
+            <img src={Instagram} alt="Instagram" className="w-6 h-6" />
           </a>
           <a
             href="https://github.com/Favourshaw/"
-            className="flex items-center justify-center w-1/4 h-full transition hover:bg-white/70 hover:scale-110"
+            className="flex items-center justify-center w-1/5 h-full transition hover:bg-gray-500 hover:scale-110"
           >
-            <img src={Git} alt="Git" className="w-6 h-6" />
+            <img src={Git} alt="GitHub" className="w-6 h-6" />
+          </a>
+          <a
+            href="mailto:favourshaw@gmail.com"
+            className="flex items-center justify-center w-1/5 h-full transition hover:bg-white/70 hover:scale-110"
+          >
+            <img src={Gmail} alt="Gmail" className="w-6 h-6" />
           </a>
         </div>
       </div>
